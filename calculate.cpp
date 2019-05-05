bc9ed9c54d045664d02a7e857e26fa0ae4dd1f08
orshemesh <chikos2011@gmail.com>
2019-05-05 16:06:50 +0300

    calculate

--- calculate.cpp
+++ calculate.cpp
@@ -4,30 +4,21 @@
 using namespace std;
 namespace bullpgia {
 string calculateBullAndPgia(string choice,string guess){
-        int length  =choice.length();
-        bool * cheack = new bool[length];
-        for (size_t i = 0; i < length; i++) {
-                cheack[i]=false;
-        }
- 
-        int bull=0;
-        int pgia=0;
- 
-        for (size_t i = 0; i < length; i++) {
-                if(choice.at(i)==guess.at(i)) {
+        int bull = 0;
+        int pgia = 0;
+        int num[10] = {0};
+        for(int i=0; i<choice.length();; i++)
+        {
+                if(choice.at(i) == guess.at(i))
+                {
                         bull++;
-                        cheack[i]=true;
+                }
+                else
+                {
+                        if(num[guess.at(i) - '0']++ < 0) pgia++;
+                        if(num[choice.at(i) - '0']-- > 0) pgia++;
                 }
         }
-        for (size_t i = 0; i < length; i++) {
-                for (size_t j = 0; j < length; j++) {
-                        if(choice.at(i)==guess.at(j)&&!cheack[i]&&!cheack[j]) {
-                                pgia++;
-                                break;
-                        }
-                }
-        }
-        delete[] cheack;
-        return to_string(bull)+","+to_string(pgia);
+        return to_string(bull) + "," +to_string(pgia);
 }
 }
